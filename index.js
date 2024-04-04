0eb766479ff5fa11a555f50e648711471fb45737
rub walter <rub@no.email>
2024-04-04 23:27:19 +1100

    turn off token caching

--- index.js
+++ index.js
@@ -133,7 +133,8 @@ import { HttpProxyAgent } from 'http-proxy-agent';
     //check if we have existing refresh token
     let user = await dbController.getUser(username);
 
-    if (user && user.access_token && user.access_token_expire_timestamp > Utils.getUnixTime() + 300) {
+    //temporarily disable token caching, sometimes pogo rejects a valid *old* token. TMS will always attempt to get a fresh token for now
+    if (false && user && user.access_token && user.access_token_expire_timestamp > Utils.getUnixTime() + 300) {
       //return the last access_token if we have more than 5 minutes of use left
       let timeLeft = Math.floor((user.access_token_expire_timestamp - Utils.getUnixTime()) / 60);
       console.log(`[${username}] Returning existing access token with ${timeLeft} minutes left`);
